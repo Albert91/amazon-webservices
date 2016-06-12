a131f3bd0556e0cd816e835060d84f33b59d20e3
Arek <areq212@gmail.com>
2016-06-12 22:32:16 +0200

    - Popraweczki

--- src/config.py
+++
@@ -1,7 +0,0 @@
-MAIL_HOST = "smtp.googlemail.com"
-MAIL_PORT = 465
-MAIL_TLS = False
-MAIL_USERNAME = 'tomnow123456789'
-MAIL_PASSWORD = 'tomcio1234tomcio'
-MAIL_SSL = True
-QUEUE_NAME = 'arek-album'
